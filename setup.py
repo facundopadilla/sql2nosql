1d8353940bb8bf2b9147d36851df3b0cca541075
Facundo <facundo.padilla@blimop.tech>
2021-06-25 22:58:16 -0300

    :beers: The PyPi page works correctly

--- setup.py
+++ setup.py
@@ -1,28 +1,38 @@
-from distutils.core import setup
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-  name = 'sql2nosql',         # How you named your package folder (MyLib)
-  packages = ['sql2nosql'],   # Chose the same as "name"
-  version = '0.3.2',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Migrate data from SQL to NoSQL easily',   # Give a short description about your library
-  author = 'Facundo Padilla',                   # Type in your name
-  author_email = 'facundo.padilla@outlook.com',      # Type in your E-Mail
-  url = 'https://github.com/facundopadilla/sql2nosql',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/facundopadilla/sql2nosql/archive/refs/heads/main.zip',    # I explain this later on
-  keywords = ['sql', 'nosql', 'migrator', 'sql2nosql', 'migrate'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'sql2nosql',
+  packages = ['sql2nosql'],
+  version = '0.3.31',
+  license='MIT',
+  description = 'Migrate data from SQL to NoSQL easily',
+  author = 'Facundo Padilla',
+  author_email = 'facundo.padilla@outlook.com',
+  url = 'https://github.com/facundopadilla/sql2nosql',
+  download_url = 'https://github.com/facundopadilla/sql2nosql/archive/refs/heads/main.zip',
+  keywords = ['sql', 'nosql', 'migrator', 'sql2nosql', 'migrate'],
+  install_requires=[
           'simplejson',
           'tqdm',
       ],
   classifiers=[
-    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
   ],
+  include_package_data=True,
+  long_description=long_description,
+  long_description_content_type='text/markdown'
 )
